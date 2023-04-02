bb9fb545f424e3727f8c0138ccc6f16a14e8df16
julia <julia.navarro@clever-cloud.com>
2023-04-03 01:22:36 +0200

    set conversion type

--- README.md
+++ README.md
@@ -1,19 +1,30 @@
 
-# Fahrenheit to Celsius temperatures converter
+# Unofficial Sean Paul's temperature converter
 
-This simple program converts Fahrenheit temperatures to Celsius. It's one of the Rust Book suggested activities in [Control Flow](https://doc.rust-lang.org/book/ch03-05-control-flow.html) chapter.
+This simple program converts temperatures between Fahrenheit and Celsius as it ouputs "Temperature" lyrics by Sean Paul.
 
-## Workflow
+## Origins
 
-Here are some delails about Rust learning curve.
+It's one of the Rust Book suggested activities in [Control Flow](https://doc.rust-lang.org/book/ch03-05-control-flow.html) chapter.
 
-### Primitive code
+### Workflow
+
+Here are some delails about Rust learning curve, for the culture.
+
+#### Getting Started
 
 First and primitive version of the code was written after reading the [Numeric Operations](https://doc.rust-lang.org/book/ch03-02-data-types.html#numeric-operations). The difficulty was to find a way to perfome a numeric operation from a string.
 
-## Converting a String
+#### Setting up the number parameters and limiting decimals output
 
-Since we can't just perfom numeric operations from strings, we need to convert it to another type of data, in our case, an `f64` integrer (which is a number without a fractional component).
+Since we can't just perfom numeric operations from strings, we need to convert it to another type of data, in our case, an `f32` scalar, which is a number with a decimal.
+
+The number of decimals is limited to one using this parameter, because no one cares about getting ten decimals of a temperature, tbh:
+
+```rust
+ println!("Your temperature is {:.1}° Celsius", converted_temperature);
+```
+#### Getting input from a new String
 
 In this case, the `parse`method was used to convert a string to another type. This will only work on characters that can logically be converted into numbers. More details can be found in the [Rust Book](https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html?highlight=parse#comparing-the-guess-to-the-secret-number).
 
--- src/main.rs
+++ src/main.rs
@@ -6,40 +6,95 @@ fn main() {
      println!("| Let's convert a temperature! |");
      println!("=========================================\n");
     
+     println!("Unofficially sponsored by");
+     println!(r"  .-')       ('-.      ('-.          .-') _           _ (`-.     ('-.                             
+11 +      ( OO ).   _(  OO)    ( OO ).-.     ( OO ) )         ( (OO  )   ( OO ).-.                         
+12 +     (_)---\_) (,------.   / . --. / ,--./ ,--,'         _.`     \   / . --. /  ,--. ,--.    ,--.      
+13 +     /    _ |   |  .---'   | \-.  \  |   \ |  |\        (__...--''   | \-.  \   |  | |  |    |  |.-')  
+14 +     \  :` `.   |  |     .-'-'  |  | |    \|  | )        |  /  | | .-'-'  |  |  |  | | .-')  |  | OO ) 
+15 +      '..`''.) (|  '--.   \| |_.'  | |  .     |/         |  |_.' |  \| |_.'  |  |  |_|( OO ) |  |`-' | 
+16 +     .-._)   \  |  .--'    |  .-.  | |  |\    |          |  .___.'   |  .-.  |  |  | | `-' /(|  '---.' 
+17 +     \       /  |  `---.   |  | |  | |  | \   |          |  |        |  | |  | ('  '-'(_.-'  |      |  
+18 ~      `-----'   `------'   `--' `--' `--'  `--'          `--'        `--' `--'   `-----'     `------'  ");
+    
     loop {
-        // Gets input from user
-        println!("Please input a temperature");
+        // Display conversion types
+        println!("============================");
+        println!("I got the right temperature to shelter you from the storm, baby gal oh");
+        println!("Choose your conversion type");
+        println!("============================");
+        println!("1. Fahrenheit to Celsius");
+        println!("2. Celsius to Fahrenheit");
+        println!("===========================");
+        println!("0. EXIT");
 
-        let mut temperature = String::new();
+        // Set an index from 0 to 2 to limit user inputs
+        // and match the index with conversion variables
+        let conversion_type = [0, 1, 2];
+        let fahrenheit_to_celsius = conversion_type[1];
+        //let celsius_to_fahrenheit = conversion_type[2]; // unused in this scope
+        let exit = conversion_type[0];
         
-        // Reads the input
-        io::stdin().read_line(&mut temperature)
-        .expect("Failed to read line.");
+        // Get user desired conversion type
+        let mut choice = String::new();
+        
+        // Read user input
+        io::stdin()
+        .read_line(&mut choice)
+        .expect("Failed to read line");
 
-        // Parses the new string to interpret as a f32 number.
-        // This is necessary for any input, to perfom mathematic operations from an inputed number.
+        // Make sure it's a number and that it matches the index
+        let choice: usize = choice
+        .trim()
+        .parse()
+        .expect("Please enter a number from 0 to 2");
+
+
+        // Set 2 conditions depending on user input
+        if choice == exit {
+            println!("Byyyyyyye, Felicia 👋 👋 👋");
+            break;
+        } else {
+            
+            println!("---------------------------");
+            println!("Please input a temperature");
+        }
+        
+        // Get user temperature
+        let mut temperature = String::new();
+
+        // Read user input
+        io::stdin()
+        .read_line(&mut temperature)
+        .expect("Failed to read line");
+
+        // Parse string to be able to perfom a mathematic operation on it
         let temperature: f32 = match temperature.trim().parse() {
             
-            // Makes sure the input is a number
+            // Make sure the input is a number
             Ok(num) => num,
             Err(_) => continue
         };
+
+       
+            // Permorm the conversion according to matching variables
+            if choice == fahrenheit_to_celsius {
+            let converted_temperature = (temperature-32.0) / 1.8;
+            println!("I got the remedy to make you de-stress out....");
+            println!("Your temperature is {:.1}° Celsius", converted_temperature);
+
+            } else {
+                let converted_temperature = temperature * 1.8 + 32.0;
+                println!("Bumper exposed and gal you got your chest out but you no wasters cause gal");
+                println!("your temperature is {:.1}° Fahrenheit", converted_temperature);
+            } 
+       };
+
         
-        //Returns the converted temperature
-        println!("Fahrenheit temperature is {:.1}", celsius_to_fahrenheit(temperature));
-        println!("Celsius temperature is {:.1}", fahrenheit_to_celsius(temperature));
+
+    
+
     }
 
-}
 
-// Converts Celsius to Fahrenheit
-fn celsius_to_fahrenheit(temperature: f32) -> f32 {
-    let temperature = temperature * 1.8 + 32.0;
-    return temperature;
-}
 
-// Converts Fahrenheit to Celsius
-fn fahrenheit_to_celsius(temperature: f32) -> f32 {
-    let temperature = (temperature-32.0) / 1.8;
-    return temperature;
-}
